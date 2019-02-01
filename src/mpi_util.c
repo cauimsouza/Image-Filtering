942009f8df5e26ac3e0fbea90b6c87fb4804c9d9
Cauim de Souza Lima <cauimsouza@gmail.com>
2019-02-01 22:32:35 +0100

    Remove unused filter gray_line

--- src/mpi_util.c
+++ src/mpi_util.c
@@ -104,27 +104,6 @@ mpi_apply_gray_filter( animated_gif * image )
     }
 }
 
-void mpi_apply_gray_line( animated_gif * image ) 
-{
-    int i, j, k ;
-    pixel ** p ;
-
-    p = image->p ;
-
-    for ( i = 0 ; i < image->n_images ; i++ )
-    {
-        for ( j = 0 ; j < 10 ; j++ )
-        {
-            for ( k = image->width[i]/2 ; k < image->width[i] ; k++ )
-            {
-            p[i][CONV(j,k,image->width[i])].r = 0 ;
-            p[i][CONV(j,k,image->width[i])].g = 0 ;
-            p[i][CONV(j,k,image->width[i])].b = 0 ;
-            }
-        }
-    }
-}
-
 void
 mpi_apply_blur_filter( animated_gif * image, int size, int threshold )
 {
