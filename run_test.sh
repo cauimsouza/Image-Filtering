9906ceab88b6f62eeff3cb766d70b939d68f41fd
Cauim de Souza Lima <cauim.de-souza-lima@polytechnique.edu>
2019-02-13 17:33:47 +0100

    Change run_test.sh for usage with MPI

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make
+make -C build
 
 INPUT_DIR=images/original
 OUTPUT_DIR=images/processed
@@ -10,5 +10,5 @@ for i in $INPUT_DIR/*gif ; do
     DEST=$OUTPUT_DIR/`basename $i .gif`-sobel.gif
     echo "Running test on $i -> $DEST"
 
-    ./sobelf $i $DEST
+    mpirun -n "$1" ./build/filtering $i $DEST
 done
